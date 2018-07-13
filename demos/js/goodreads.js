dfadfd815c47a0ae6dde1bb4f977aff5fb563482
Vijaya Kumar Vulchi <vulchi.vijay@hotmail.com>
2018-07-13 23:00:48 +0530

    goodread api clean and functionalities done 90%

--- demos/js/goodreads.js
+++ demos/js/goodreads.js
@@ -8,57 +8,61 @@ $(function () {
   // variables
   let searchBtn = $('#search_btn');
   let searchInput = $('#search_input');
-  let apiKey = '9ZVefZRKvvSiLfY7f3pQ';
-  let searchType = 'title'; // title, author by default 'all'
   let searchResults = $('#search_result');
-  let count = $('#search_result_count');
-  let response = $('#search_result_response');
+  let totalBooks = $('#search_result_count');
+  let paginationControls = $('#pagination_controls');
+  let paginationPrev = $('#pagination_prev');
+  let paginationNext = $('#pagination_next');
+  let maxPages;
+  // api variables
+  let booksPage = 1;
+  let apiKey = '9ZVefZRKvvSiLfY7f3pQ';
+  let searchType = 'title';
   // error variables
   let inputTaskError = $('#input_task_error');
   let searchResultErrror = $('#search_result_error');
-
+  // input focus remove error message.
   searchInput.on('focus', function () {
     inputTaskError.text('');
   });
-
+  // showing all books by default
+  apiCall('1', booksPage, apiKey, 'all');
   // input on change function
   searchInput.on('change', function () {
+    booksPage = 1;
+    paginationControls.removeClass('is-enable');
     inputTaskError.text('');
     searchResults.empty();
-    count.text('0');
-    response.text('0');
-    // apiCall(searchInput, apiKey, searchType);
+    totalBooks.text('0');
   });
   // search button click function
   searchBtn.on('click', function (e) {
     if (searchInput.val() !== '') {
-      apiCall(searchInput.val(), apiKey, searchType);
-      searchInput.val('');
+      apiCall(searchInput.val().replace(/ +/g, '-'), booksPage, apiKey, searchType);
+      // searchInput.val('');
     } else {
       inputTaskError.text('This field can not be empty');
     }
     e.preventDefault();
   });
   // apicall function
-  function apiCall (input, key, type) {
-    var url = `https://www.goodreads.com/search/index.xml?q=${input.replace(/ +/g, '-')}&key=${key}&search=${type}`;
+  function apiCall (input, page, key, type) {
+    var url = `https://www.goodreads.com/search/index.xml?q=${input}&page=${page}&key=${key}&search=${type}`;
     $.get('https://query.yahooapis.com/v1/public/yql',
       {
-        // "select * from xml where url=\""+url+"\"",
         q: `select * from xml where url="${url}"`,
         format: 'xml'
       },
       function (xml) {
-        // console.log(xml);
+        // clearing exising search results
+        searchResults.empty();
         let data = xmlToJson(xml);
         let books = data['query']['results']['GoodreadsResponse']['search']['results']['work'];
+        let allBooks = data['query']['results']['GoodreadsResponse']['search']['total-results']['#text'];
         if (books) {
+          paginationControls.addClass('is-enable');
           searchResultErrror.text('');
-          count.text(books.length);
-          response.text(data['query']['results']['GoodreadsResponse']['search']['query-time-seconds']['#text']);
-          console.log(books);
-          console.log(data['query']);
-          // response.text(books.)
+          totalBooks.text(allBooks);
           books.forEach(function (item, index) {
             let bookDiv = $('<div class="col-3"></div>');
             let bookTitle = item['best_book']['title']['#text'];
@@ -67,13 +71,49 @@ $(function () {
             bookDiv.append(figureTag);
             searchResults.append(bookDiv);
           });
+          paginationController();
         } else {
+          paginationControls.removeClass('is-enable');
           searchResultErrror.text('No books found! Please try again.');
         }
       }
     );
   }
-
+  function paginationController () {
+    maxPages = Math.ceil(parseInt(totalBooks.text()) / 20);
+    if (booksPage === 1) {
+      paginationPrev.addClass('is-disable');
+    }
+    paginationNext.unbind('click').click(function () {
+      booksPage++;
+      if (booksPage === maxPages) {
+        paginationPrev.removeClass('is-disable');
+        $(this).addClass('is-disable');
+        callApiAgain();
+      } else if (booksPage < maxPages) {
+        paginationPrev.removeClass('is-disable');
+        callApiAgain();
+      } else {
+        $(this).addClass('is-disable');
+      }
+    });
+    paginationPrev.unbind('click').click(function () {
+      booksPage--;
+      if (booksPage > 0) {
+        paginationNext.removeClass('is-disable');
+        callApiAgain();
+      } else {
+        $(this).addClass('is-disable');
+      }
+    });
+    function callApiAgain () {
+      if (searchInput.val() === '') {
+        apiCall('1', booksPage, apiKey, 'all');
+      } else {
+        apiCall(searchInput.val(), booksPage, apiKey, searchType);
+      }
+    }
+  }
   // Changes XML to JSON
   function xmlToJson (xml) {
     var obj = {};
